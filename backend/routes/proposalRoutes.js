750e11da4386637e7b52f810dfd1251d669b2e6c
Palleda Lokesh <160224924+lokeshpalleda@users.noreply.github.com>
2026-03-29 12:41:27 +0530

    Fix case of Proposal model import

--- backend/routes/proposalRoutes.js
+++ backend/routes/proposalRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Proposal = require("../models/proposal");
+const Proposal = require("../models/Proposal");
 
 const {
   createProposal,
