250626a139c94b99c7b4d9f7a5c42906360a2672
Palleda Lokesh <160224924+lokeshpalleda@users.noreply.github.com>
2026-03-29 14:46:19 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://gyaansetu-backend-5eez.onrender.com"
+  baseURL: "https://gyaansetu-backend-5eez.onrender.com/api"
 });
 
 export default API;
